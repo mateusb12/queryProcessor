0428d81a46efb15c1ce22196e6ecf28a1dbbef6b
mateusb12 <mateus_b09@hotmail.com>
2022-10-21 11:59:29 -0300

    translator 2

--- queries/relational_algebra_translator.py
+++ queries/relational_algebra_translator.py
@@ -1,3 +1,4 @@
+import random
 from functools import reduce
 
 from queries.relational_algebra_splitter import get_split_example
@@ -9,23 +10,27 @@ class RelationalAlgebraTranslator:
 
     def translate(self):
         projection = self.extract_projection()
-        cartesian_product = self.extract_cartesian_product()
         selection = self.extract_selection()
+        cartesian_product = self.extract_cartesian_product()
+        full_expression = f"{projection}({selection}({cartesian_product}))"
         return 0
 
     def extract_projection(self):
-        return ["π(" + ", ".join(self.split_info["SELECT"]) + ")"]
-
-    def extract_cartesian_product(self):
-        from_values = self.split_info["FROM"]
-        # Create a lambda function to join two strings and form a cartesian product
-        join = lambda x, y: f"({x} ⨯ {y})"
-        # Use reduce with join to form the cartesian product
-        return [reduce(join, from_values)]
+        return ["π[" + ", ".join(self.split_info["SELECT"]) + "]"][0]
 
     def extract_selection(self):
         where_values = self.split_info["WHERE"]
-        return [f"σ({item[0]}{item[1]}{item[2]})" for item in where_values]
+        def selection_notation(item): return f"σ[{item[0]}{item[1]}{item[2]}]"
+        new_where_values = [selection_notation(item) for item in where_values]
+        def selection_two_items(x, y): return f"{x} ^ {y}"
+        return reduce(selection_two_items, new_where_values)
+
+    def extract_cartesian_product(self):
+        from_values = self.split_info["FROM"]
+        def cartesian_join(x, y): return f"({x} ⨯ {y})"
+        return reduce(cartesian_join, from_values)
+
+
 
 
 def __main():
