e948609116013e8b82966be8cd79a92c3b35d51e
mateusb12 <mateus_b09@hotmail.com>
2022-10-25 21:08:27 -0300

    graph with networkx

--- queryTree/network_tree.py
+++ queryTree/network_tree.py
@@ -1,14 +1,27 @@
 from queryTree.tree import Tree, build_example_tree
+import matplotlib.pyplot as plt
+import networkx as nx
 
 
 class NetworkNxTree:
     def __init__(self, input_tree: Tree):
         self.tree = input_tree
+        self.edges = self.tree.edge_pot
+        self.graph = nx.Graph()
+        self.graph.add_edges_from(self.edges)
+
+    def draw_graph(self):
+        nx.draw(self.graph, with_labels=True)
+        ax = plt.gca()
+        plt.show()
+        return
 
 
 def __main():
     example_tree = build_example_tree()
     nxt = NetworkNxTree(example_tree)
+    nxt.draw_graph()
+    pass
 
 
 if __name__ == "__main__":
