3482c9a8e7f1c1549770173acc4e609fb69f2d38
mateusb12 <mateus_b09@hotmail.com>
2022-10-28 14:10:36 -0300

    cross product optimizer

--- optimizer/query_optimizer.py
+++ optimizer/query_optimizer.py
@@ -5,15 +5,38 @@ from queries.relational_algebra_wrapper import relational_algebra_wrapper
 class QueryOptimizer:
     def __init__(self, instruction_list: list[str]):
         self.instructions = instruction_list
-        self.cross_product = False
+        self.instruction_dict = {}
+        self.optimizer_pipeline()
+
+    def optimizer_pipeline(self):
+        self.__analyze_instructions()
+        if "cross_product" in self.instruction_dict.keys():
+            self.avoid_cross_product()
 
     def __analyze_instructions(self):
         for item in self.instructions:
             if "⨯" in item:
-                self.cross_product = True
+                self.instruction_dict["cross_product"] = item
 
     def avoid_cross_product(self):
-        pass
+        main_value = self.instruction_dict["cross_product"]
+        table_a, table_b = [item.replace(" ", "") for item in main_value.split("⨯")]
+        removed_instruction, new_instruction = "", ""
+        for instruction in self.instructions:
+            if instruction.startswith("σ") and table_a in instruction and table_b in instruction:
+                operators = ["=", "<", ">", "<=", ">=", "<>"]
+                existing_operator = [operator for operator in operators if operator in instruction][0]
+                raw_condition = instruction.split(existing_operator)
+                left_condition, right_condition = [item.replace("σ[", "").replace("]", "") for item in raw_condition]
+                removed_instruction = instruction
+                new_instruction = f"({table_a} |X| •{left_condition}{existing_operator}{right_condition}• {table_b})"
+                break
+        self.instructions.remove(removed_instruction)
+        for index, instruction in enumerate(self.instructions):
+            if instruction == main_value:
+                self.instructions[index] = new_instruction
+                break
+        return
 
 
 def __main():
